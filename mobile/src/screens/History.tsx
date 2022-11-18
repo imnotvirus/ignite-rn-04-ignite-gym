00abb99b6c21296baa2f884c9f09cd0b5e9b1d30
RennanD <rplaykl@gmail.com>
2022-11-18 16:19:51 -0300

    Refresh Token - Exibindo mensagem quando não há histórico

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
+import { Heading, VStack, SectionList, Text, useToast, Center } from 'native-base';
 
 import { useAuth } from '@hooks/useAuth';
 
@@ -49,28 +49,31 @@ export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
-
       {
-        isLoading ? <Loading /> : exercises?.length &&
-          <SectionList 
-            sections={exercises}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => <HistoryCard data={item} /> }
-            renderSectionHeader={({ section }) => (
-              <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
-                {section.title}
-              </Heading>
-            )}
-            px={8}
-            contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
-            ListEmptyComponent={() => (
+        isLoading ? <Loading /> : (
+          exercises?.length > 0 ?
+            <SectionList 
+              sections={exercises}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <HistoryCard data={item} /> }
+              renderSectionHeader={({ section }) => (
+                <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
+                  {section.title}
+                </Heading>
+              )}
+              px={8}
+              contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+              showsVerticalScrollIndicator={false}
+            />
+          :
+            <Center flex={1}>
               <Text color="gray.100" textAlign="center">
                 Não há exercícios registrados ainda. {'\n'}
                 Vamos fazer exercícios hoje?
               </Text>
-            )}
-            showsVerticalScrollIndicator={false}
-        />
+            </Center>
+        )
+        
       }
 
 
